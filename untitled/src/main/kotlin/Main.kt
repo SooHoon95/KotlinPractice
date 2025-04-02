ebc842f898b87dc9ff0eb54d8b7669c2d8ee40cc
최수훈 <81559117+SooHoon95@users.noreply.github.com>
2025-04-02 10:25:04 +0900

    상수 , 변수

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name: String = "devLifter" // 상수
+    var age: Int = 31
+
+    println("이름 : $name, 나이 : $age")
+    age = 32
+//    name = "devLifterrrrr" // 상수 변경 불가
+    println("일년 후 나이 : $age")
+}
